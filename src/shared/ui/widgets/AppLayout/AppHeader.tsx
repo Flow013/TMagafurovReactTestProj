4634e8ba34691bd4a08ee74303a92dfcdb630452
Тalgat Magafurov <TMagafurov@nvtech.ru>
2023-04-27 21:24:17 +0500

    remove menu

--- src/shared/ui/widgets/AppLayout/AppHeader.tsx
+++ src/shared/ui/widgets/AppLayout/AppHeader.tsx
@@ -1,21 +1,21 @@
 import { Col, Layout, Menu, MenuProps, Row } from 'antd'
 import { LngSelector } from '../LngSelector'
 
-const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
-  key,
-  label: `nav ${key}`
-}))
+// const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
+//   key,
+//   label: `nav ${key}`
+// }))
 
 export const AppHeader = () => (
   <Layout.Header>
     <Row justify="space-between">
       <Col>
-        <Menu
+        {/* <Menu
           theme="dark"
           mode="horizontal"
           defaultSelectedKeys={['2']}
           items={items1}
-        />
+        /> */}
       </Col>
       <Col>
         <LngSelector />
